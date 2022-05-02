990ab97df4709ff809f3bf65ea09146f481da1df
ismaeltovar <73203513+ismaeltovar@users.noreply.github.com>
2022-05-02 16:00:27 -0500

    Added index.js in components folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,7 @@
 //Export & Import of components
+
+export {default as GridSquare} from './GridSquare/GridSquare';
+export {default as Board} from './Board/Board';
+export {default as CompBoard} from './Board/CompBoard';
+export {default as UserBoard} from './Board/UserBoard';
+export {default as shipsGenerator} from './shipsGenerator/shipsGenerator';
