fd4de66594bb3d4582810ade036eb0bb0cbbcb07
ismaeltovar <73203513+ismaeltovar@users.noreply.github.com>
2022-05-09 20:46:29 -0500

    Updated App.js with major functionality

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,62 @@
-import './App.css';
+import React, { Component, createContext } from 'react';
+import { CompBoard, shipsGenerator, UserBoard } from '../components';
+import consoleHelper from '../consoleHelper';
+import cpuAttack from '../cpuAttack/cpuAttack';
+import '../sass/styles.js';
 
-function App() {
+export const ShipsContext = createContext();
+export const AttackContext = createContext();
+export let userShips = [];
+export let compShips = [];
+
+class App extends Component {
+constructor(props) {
+  super(props);
+
+  this.state = {
+    userTurn: true,
+    userSqurAttacked: [],
+    compSqurAttacked: [],
+    attacked: this.attacked,
+  }
+}
+
+attacked = (e) => {
+  e.preventDefault();
+  const coordinate = e.target.id;
+  const board = e.target;
+  consoleHelper(`Target: ${board}\tCoordinate: ${coordinate}`);
+  this.setState((state) => ({
+    userSqurAttacked: [...state.userSqurAttacked, cpuAttack(state.userSqurAttacked)],
+    compSqurAttacked: [...state.compSqurAttacked, coordinate]
+  }));
+  // consoleHelper(`User SQ att: ${this.state.userSqurAttacked}`);
+  consoleHelper(`Comp SQ att: ${this.state.compSqurAttacked}`);
+  
+}
+
+componentDidMount() {
+  userShips = shipsGenerator('USER');
+  compShips = shipsGenerator('CPU');
+}
+
+componentDidUpdate() {
+  consoleHelper('App update');
+  consoleHelper(`Comp SQ att: ${this.state.compSqurAttacked}`);
+}
+
+render() {
   return (
     <div className="App">
-
+      <main id='boards'>
+        <AttackContext.Provider value={this.state}>
+          <UserBoard/>
+          <CompBoard/>
+        </AttackContext.Provider>
+      </main>
     </div>
   );
+  }
 }
 
 export default App;
