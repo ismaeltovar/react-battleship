76fa8ab39fcb656d5bae9a6d4951c1d2cc4c8c28
ismaeltovar <73203513+ismaeltovar@users.noreply.github.com>
2022-05-11 18:43:59 -0500

    Improved GridSquare

--- src/components/GridSquare/GridSquare.js
+++ src/components/GridSquare/GridSquare.js
@@ -1,30 +1,20 @@
 import React, { Component } from "react";
+import consoleHelper from "../../consoleHelper";
 
 export default class GridSquare extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			attacked: false,
-			disabled: false
 		}
-		this.attacked = this.props.attacked;
-		//this.attackInfo = this.props.attackInfo;
-		this.id = this.props.id;
-		this.humanPlayer = this.props.humanPlayer;
-		this.shipHere = this.props.shipHere;
-		this.clickHandler = this.clickHandler.bind(this);
-	}
-
-	clickHandler(e) {		
-		this.setState({attacked: true, disabled: true});
 	}
 
 	render() {
 		return(
-			<td className="g-squr">
-				<button className={`g-btn ${this.state.attacked && this.shipHere ? 'att-ship' : this.state.attacked ? 'att-no-ship': 'no-att'}`} 
-				onClick={this.clickHandler} 
-				disabled={this.humanPlayer ? true : this.state.disabled}></button>
+			<td key={this.props.attacked} className="g-squr">
+				<button id={this.props.id} className={`g-btn ${this.props.attacked && this.props.shipHere ? 'att-ship' : this.props.attacked ? 'att-no-ship': 'no-att'}`} 
+				onClick={this.props.onAttack.bind(this)}
+				disabled={this.props.humanPlayer ? true : this.props.attacked}></button>
 			</td>
 		);
 	}
