09f5031971b4338a5e53fc6e9f503ddf68a354db
ismaeltovar <73203513+ismaeltovar@users.noreply.github.com>
2022-05-26 14:28:01 -0500

    Improved shipsGenerator & bug fix

--- src/components/shipsGenerator/shipsGenerator.js
+++ src/components/shipsGenerator/shipsGenerator.js
@@ -30,74 +30,143 @@ export default function shipsGenerator(boardType) {
 function getRandomShipLocal(length, invCoordinates) {
   let foundValidLoc = false;
   let ship = [];
-  let xStart;
-  let yStart;
-  let direction;
-  let loopNum; //For debugging purposes
-
+  let loopNum = 0; //For debugging purposes
+  
+  /** Clock-wise rotation for following loop
+   * 0 = up
+   * 1 = right
+   * 2 = down
+   * 3 = left
+   */
+  
   while(!foundValidLoc) {
     if (loopNum > 1500)
       console.error('Too many loops in getRandomShipLocal. Please refresh the page.');
     consoleHelper('Finding valid location');
     loopNum++;
-    xStart = Math.floor(Math.random() * (10 - length - 1));
-    yStart = Math.floor(Math.random() * (10 - length - 1));
+    let directions = [0, 1, 2, 3];
+    let directionIndex = undefined;
+    let columnStart = Math.floor(Math.random() * 10);
+    let rowStart = Math.floor(Math.random() * 10);
+
+    let sqrsNeeded = length - 1;
+
+    if (rowStart - sqrsNeeded < 0) {
+      directions = directions.filter((value) => {
+        return value !== 0;
+      });
+    }
+    if (columnStart + sqrsNeeded > 9) {
+      directions = directions.filter((value) => {
+        return value !== 1;
+      });
+    }
+    if (rowStart + sqrsNeeded > 9) {
+      directions = directions.filter((value) => {
+        return value !== 2;
+      });
+    }
+    if (columnStart - sqrsNeeded < 0) {
+      directions = directions.filter((value) => {
+        return value !== 3;
+      });
+    }
+
     
-    let sqrsLeft = length;
-      /** Clock-wise rotation for following loop
-       * 0 = up
-       * 1 = right
-       * 2 = down
-       * 3 = left
-        */
-    for (let rotation = 0; rotation < 4; rotation++) {
+    invCoordinates.forEach(coor => {
+      let columnCoor = cHeaders.findIndex(h => h === coor.charAt(0));
+      let rowCoor = parseInt(coor.charAt(1));
+      if (rowCoor >= rowStart - length && rowCoor <= rowStart + 1 && columnCoor === columnStart && directions.find(el => el === 0) !== undefined)
+        directions = directions.filter((value) => {
+          return value !== 0;
+        });
+        //Here's were I left off.
+      else if (columnCoor <= columnStart + length && columnCoor >= columnStart - 1 && rowCoor === rowStart && directions.find(el => el === 1) !== undefined)
+        directions = directions.filter((value) => {
+          return value !== 1;
+        });
+      else if (rowCoor <= rowStart + length && rowCoor >= rowStart - 1 && columnCoor === columnStart && directions.find(el => el === 2) !== undefined)
+        directions = directions.filter((value) => {
+          return value !== 2;
+        });
+      else if (columnCoor >= columnStart - length && columnCoor <= columnStart + 1 && rowCoor === rowStart && directions.find(el => el === 3) !== undefined)
+        directions = directions.filter((value) => {
+          return value !== 3;
+        });
+    });
 
-      for (let sqrsBuilt = 0; sqrsBuilt < length; sqrsBuilt++) {
-        let coordinate;
-        if (rotation === 0)
-          coordinate = `${cHeaders[xStart]}${yStart + sqrsBuilt}`;
-        else if (rotation === 1)
-          coordinate = `${cHeaders[xStart + sqrsBuilt]}${yStart}`;
-        else if (rotation === 2)
-          coordinate = `${cHeaders[xStart]}${yStart - sqrsBuilt}`;
-        else if (rotation === 3)
-          coordinate = `${cHeaders[xStart - sqrsBuilt]}${yStart}`;
-        else {
-          console.error('Invalid rotation in ship location check loop in getRandomShipLocal');
-        }
+    consoleHelper(`Directions: ${directions.toString()}`);
+    
+    if (directions.length === 0)
+      break;
+    else 
+      foundValidLoc = true;
+    consoleHelper(`FoundValidLoc: ${foundValidLoc}`);
 
-        if (invCoordinates.find(coor => coordinate === coor) === undefined) {
-          sqrsLeft--;
-          continue;
-        }
-        else
-          break;
-      }
+    directionIndex = directions[Math.floor(Math.random() * directions.length)];
+      
+    ship.push(`${cHeaders[columnStart]}${rowStart}`);
+    consoleHelper(`Start coordinate: ${ship[0]}`);
 
-      if (sqrsLeft === 0) {
-        foundValidLoc = true;
-        direction = rotation;
-        break;
-      }
+    for (let i = 1; i < length; i++) {
+      if (directionIndex === 0)
+        rowStart--;
+      else if (directionIndex === 1)
+        columnStart++;
+      else if (directionIndex === 2)
+        rowStart++;
+      else if (directionIndex === 3)
+        columnStart--;
+      ship.push(`${cHeaders[columnStart]}${rowStart}`);
     }
   }
 
-  for (let i = 0; i < length; i++) {
-    let coordinate;
-    if (direction === 0)
-      coordinate = `${cHeaders[xStart]}${yStart + i}`;
-    else if (direction === 1)
-      coordinate = `${cHeaders[xStart + i]}${yStart}`;
-    else if (direction === 2)
-      coordinate = `${cHeaders[xStart]}${yStart - i}`;
-    else if (direction === 3)
-      coordinate = `${cHeaders[xStart - i]}${yStart}`;
-    else {
-      console.error('Invalid rotation in ship building loop in getRandomShipLocal');
-    }
-    ship.push(coordinate);
-  }
-  
   consoleHelper('Ship constructed: ' + ship.toString());
-  return ship; 
+  return ship;
 }
+    //For reference:     
+
+    // for (let rotation = 0; rotation < 4; rotation++) {
+
+    //   for (let sqrsBuilt = 0; sqrsBuilt < length; sqrsBuilt++) {
+    //     let coordinate;
+    //     if (rotation === 0)
+    //       coordinate = `${cHeaders[xStart]}${yStart + sqrsBuilt}`;
+    //     else if (rotation === 1)
+    //       coordinate = `${cHeaders[xStart + sqrsBuilt]}${yStart}`;
+    //     else if (rotation === 2)
+    //       coordinate = `${cHeaders[xStart]}${yStart - sqrsBuilt}`;
+    //     else if (rotation === 3)
+    //       coordinate = `${cHeaders[xStart - sqrsBuilt]}${yStart}`;
+    //     else {
+    //       console.error('Invalid rotation in ship location check loop in getRandomShipLocal');
+    //     }
+
+    //     if (invCoordinates.find(coor => coordinate === coor) === undefined) {
+    //       sqrsLeft--;
+    //       continue;
+    //     }
+    //     else
+    //       break;
+    //   }
+
+      // if (sqrsLeft === 0) {
+      //   foundValidLoc = true;
+      //   direction = rotation;
+      //   break;
+      // }
+  // for (let i = 0; i < length; i++) {
+  //   let coordinate;
+  //   if (direction === 0)
+  //     coordinate = `${cHeaders[xStart]}${yStart + i}`;
+  //   else if (direction === 1)
+  //     coordinate = `${cHeaders[xStart + i]}${yStart}`;
+  //   else if (direction === 2)
+  //     coordinate = `${cHeaders[xStart]}${yStart - i}`;
+  //   else if (direction === 3)
+  //     coordinate = `${cHeaders[xStart - i]}${yStart}`;
+  //   else {
+  //     console.error('Invalid rotation in ship building loop in getRandomShipLocal');
+  //   }
+  //   ship.push(coordinate);
+  // }
