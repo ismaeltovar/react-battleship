a655a92f3d3e022ddeb7a7a19e091c311d731828
ismaeltovar <73203513+ismaeltovar@users.noreply.github.com>
2022-06-16 09:19:42 -0500

    Added Icon component

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,5 +1,14 @@
-import { IoMenu, IoLocate } from "react-icons/io5";
+import { IoMenu, IoLocate, IoWarning, IoHelp, IoRepeat } from "react-icons/io5";
 
 export default function Icon({type}) {
-  return type === 'menu' ? <IoMenu className={`icon-c ${type}`}/> : <IoLocate className={`icon-c ${type}`}/>;
+  if (type === 'menu')
+    return <IoMenu className={`icon-c ${type}`}/>;
+  else if (type === 'locate')
+    return <IoLocate className={`icon-c ${type}`}/>;
+  else if (type === 'help')
+    return <IoHelp className={`icon-c ${type}`}/>;
+  else if (type === 'repeat')
+    return <IoRepeat className={`icon-c ${type}`}/>;
+  else 
+    return <IoWarning className={`icon-c ${type}`}/>;
 }
